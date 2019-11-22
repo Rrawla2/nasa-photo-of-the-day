c1a71ed2c539610685141477f5b137ff76e5a02c
Kristi <kgribble4083@gmail.com>
2019-11-21 22:14:02 -0600

    styled card from reactstrap

--- src/components/NasaPhoto.js
+++ src/components/NasaPhoto.js
@@ -1,37 +1,44 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import styled from 'styled-components';
-
+import { Card, CardHeader, CardImg, CardText, CardBody, CardTitle, Row, Col } from "reactstrap";
 
 function NasaPhoto() {
-    const [nasaPhoto, setNasaPhoto] = useState("");
-    const [nasaTitle, setNasaTitle] = useState("");
-    const [nasaDate, setNasaDate] = useState("");
-    const [nasaExplanation, setNasaExplanation] = useState("");
-    const [nasaCopyrightInfo, setNasaCopyrightInfo] = useState("");
+    const [nasaData, setNasaData] = useState({});
+    // const [nasaPhoto, setNasaPhoto] = useState("");
+    // const [nasaTitle, setNasaTitle] = useState("");
+    // const [nasaDate, setNasaDate] = useState("");
+    // const [nasaExplanation, setNasaExplanation] = useState("");
+    // const [nasaCopyrightInfo, setNasaCopyrightInfo] = useState("");
 
     useEffect(() => {
         axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
-            .then(response => {setNasaPhoto(response.data.url);
-            setNasaTitle(response.data.title);
-            setNasaDate(response.data.date);
-            setNasaExplanation(response.data.explanation);
-            setNasaCopyrightInfo(response.data.copyright);
+            .then(response => {setNasaData(response.data)
+            // setNasaPhoto(response.data.url);
+            // setNasaTitle(response.data.title);
+            // setNasaDate(response.data.date);
+            // setNasaExplanation(response.data.explanation);
+            // setNasaCopyrightInfo(response.data.copyright);
             })
             .catch(err => console.log("Image not found", err));
     }, []);
 
     return (
-        
-        <div className="container">
-            <h1>{ nasaTitle }</h1>
-            <h4>Today's Date: { nasaDate }</h4>
-            <img src={ nasaPhoto } alt="Nasa photo of the day"/>
-            <h6>&copy; { nasaCopyrightInfo }</h6>
-            <h3>Fun Facts</h3>
-            <p> { nasaExplanation }</p>
-        </div>
-        
+        <Row>
+        <Col sm="6">
+            <Card>
+                <CardBody>
+                    <CardHeader tag="h1">{nasaData.title}</CardHeader>
+                    <CardTitle tag="h4">Today's Date: {nasaData.date}</CardTitle>
+                </CardBody>
+                <CardImg src={nasaData.url} alt="Nasa photo of the day"/>
+                <CardBody>
+                    <CardText>&copy; {nasaData.copyright}</CardText>
+                    <CardTitle tag="h2">Fun Facts</CardTitle>
+                    <CardText> {nasaData.explanation}</CardText>
+                </CardBody>
+            </Card>
+        </Col>
+        </Row>
     )
 }
 
